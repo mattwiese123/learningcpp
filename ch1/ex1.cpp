3a3673c8d623d8f20027e57f760c329fd3ad5784
Matthew Wiese <mattwiese123@gmail.com>
2022-01-09 09:28:14 -0600

    Added ex1, cmake

---
+++ ch1/ex1.cpp
@@ -0,0 +1,13 @@
+//
+// Created by matt on 2022-01-08.
+//
+
+#import <iostream>
+
+int main() {
+    int answer = {42};
+
+    std::cout << "The answer to the problem is "
+            << answer
+            << std::endl;
+}
